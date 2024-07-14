f70c36be86ab7a63732eb0668cd9b4b347fc675f
Passeriade <manuel5mongelos@gmail.com>
2024-07-14 01:53:24 -0300

    Cors allowing

--- reacher_api.py
+++ reacher_api.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import PlainTextResponse
 from pydantic import BaseModel
 import reach_service
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Links(BaseModel):
@@ -11,7 +20,7 @@ class Links(BaseModel):
     to_link: str
 
 
-@app.post("/reach/")
+@app.post("/reach")
 def reacher(links: Links):
     from_link = links.from_link
     to_link = links.to_link

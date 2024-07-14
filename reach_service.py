6c429302d7e4e257b6f8531cd720e1c6f3f5e733
Passeriade <manuel5mongelos@gmail.com>
2024-07-14 02:02:01 -0300

    Comments

--- reach_service.py
+++ reach_service.py
@@ -6,7 +6,6 @@ import requests
 
 
 def get_url(session: requests.Session, url: str):
-    # Try to send an HTTP request
     try:
         response = session.get(url)
         response.raise_for_status()
@@ -19,10 +18,9 @@ def get_url(session: requests.Session, url: str):
 
 
 def get_links_in_page(response: requests.Response):
-    # Parse the HTML content using lxml
     tree = html.fromstring(response.content)
     div = tree.xpath("//div[@id='mw-content-text']")[0]
-    # Extract <p>
+    # Extract links from <p> that aren't buttons and not an unwanted link
     links_to_search = div.xpath(
         ".//p//a[not(@role='button') and not(starts-with(@href, '/wiki/Ayuda:'))"
         + "and not(starts-with(@href, '/w/index')) and not(starts-with(@href, '/wiki/Archivo:'))]/@href"
@@ -48,7 +46,7 @@ def scraper(
                     # as the context will always be the same
                     # It would be a win/lose between memory and time complexity
                     # link_dict[link] = url[24:] # With slicing
-                    # For the time being, I want it to be faster
+                    # For the time being, I want it to be faster rather to save memory
                     link_dict[link] = url
                     keys_links.append(link)
                 if link == searched_link:
@@ -128,6 +126,7 @@ def reach(first_link: str, end_link: str):
         path_beautified = beautify_path(path)
         end_time = datetime.now()
         return (
+            # It ended like this in order to get a quick front
             f"<b>Links encontrados:</b> {len(links_dict)}"
             + f"<br><b>Con camino más corto:</b> {path_beautified}"
             + f"<br><b>En un tiempo de:</b> {str(end_time-start_time)[:-3]}"
